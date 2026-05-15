49285e2358fa49b65b4e952b8841c0b8377e629e
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2026-05-15 20:14:42 +0500

    add the frontend url in cors origin

--- backend/main.py
+++ backend/main.py
@@ -4,11 +4,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from routers.chat_router import router as ChatRouter 
 from routers.searchchannel_router import router as SearchChannelRouter 
 from routers.downloadvideo_router import router as DownloadVideoRouter 
+from configs import env_config
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080","http://192.168.0.119:8080"],
+    allow_origins=["http://localhost:8080",f"{env_config.frontend_url}"],
     # allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],  # Allows all methods
